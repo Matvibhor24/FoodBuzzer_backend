3d1aab82628b2d3b02a46a46ab0ff9e8f5cc2d1e
Vibhor Mathur <99306756+Matvibhor24@users.noreply.github.com>
2026-03-03 23:34:37 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/example/food_buzzer_backend/FoodBuzzerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_buzzer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBuzzerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBuzzerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/food_buzzer_backend/FoodBuzzerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_buzzer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBuzzerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
